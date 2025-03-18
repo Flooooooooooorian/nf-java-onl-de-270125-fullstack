5e9d3748aef3a3d7df70ed81de6c6b744ba23c84
marcell <marcell.dechant@neuefische.de>
2025-03-18 11:03:15 +0100

    Implement first steps for OAuth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,19 +15,29 @@ function App() {
             })
     }
 
+    function getMe() {
+        axios.get("/api/auth/me")
+            .then(r => console.log(r))
+            .catch(e => console.error(e))
+    }
+
+    useEffect(getMe, []);
     useEffect(fetchTodos, [])
 
-    if (!todos) {
-        return "Lade..."
+    function login() {
+        // /oauth2/authorization/github <- google, facebook whatever drin stehen
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+        window.open(host + '/oauth2/authorization/github', '_self')
     }
 
     return (
         <>
+            <button onClick={login}>Login with Github</button>
             <div className="page">
                 <h1>All TODOs</h1>
                 {
                     allPossibleTodos.map(status => {
-                        const filteredTodos = todos.filter(todo => todo.status === status)
+                        const filteredTodos = todos ? todos.filter(todo => todo.status === status) : []
                         return <TodoColumn
                             status={status}
                             todos={filteredTodos}
