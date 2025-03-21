257853db8a94e24d98977fee2efb667d49dd31fd
marcell <marcell.dechant@neuefische.de>
2025-03-21 16:28:01 +0100

    Add testing for me endpoint

--- backend/src/test/java/de/neuefische/backend/auth/AuthControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/auth/AuthControllerTest.java
@@ -1,17 +1,22 @@
 package de.neuefische.backend.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.appuser.AppUser;
+import de.neuefische.backend.appuser.AppUserRoles;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,10 +25,30 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void getMe() throws Exception {
-        mockMvc.perform(get("/api/auth/me").with(oidcLogin().userInfoToken(token -> token.claim("login", "test-user"))))
+        AppUser appUser = AppUser.builder()
+                .id("123")
+                .username("test-user")
+                .avatarUrl("https://www.gidf.de")
+                .role(AppUserRoles.ADMIN)
+                .todoIds(List.of())
+                .build();
+        String appUserJson = objectMapper.writeValueAsString(appUser);
+
+        TestingAuthenticationToken authentication = new TestingAuthenticationToken(appUser, null, "ROLE_ADMIN");
+
+        mockMvc.perform(get("/api/auth/me")
+                        .with(oidcLogin().userInfoToken(token -> token
+                                .claim("login", "test-user")
+                                .claim("id", "123")
+                                .claim("avatar_url", "https://www.gidf.de")
+                        ))
+                        .with(SecurityMockMvcRequestPostProcessors.authentication(authentication)))
                 .andExpect(status().isOk())
-                .andExpect(content().string("test-user"));
+                .andExpect(content().json(appUserJson));
     }
 }
